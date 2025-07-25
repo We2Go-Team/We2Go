44e3ca1f8cc20d8c84e592f1db6040f75a2c8039
dev Hai <hainguyen0112358@gmail.com>
2025-07-26 04:43:37 +0700

    Update filter and search

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,7 @@
 from .home import home
 from .auth import auth
-
+from .tickets import tickets  
 def register_routes(app):
     app.register_blueprint(home)
     app.register_blueprint(auth)
+    app.register_blueprint(tickets)
